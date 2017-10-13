cad911d24ac3a1edfbfa26602f23188614d8bf64
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2017-10-13 18:25:53 +0200

    Initial commit

---
+++ Monsters hunting persistence/src/test/java/cz/fi/muni/pa165/dao/ZombieDaoImplTest.java
@@ -0,0 +1,11 @@
+package cz.fi.muni.pa165.dao;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ZombieDaoImplTest {
+
+	public void test() {
+		assertThat(false).isEqualTo(false);
+	}
+}
