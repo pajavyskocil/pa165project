041311828352902d3395148e0024164fdc9af220
balcirakpeter <peter.balcirak@gmail.com>
2017-10-27 15:48:05 +0200

    UserDAO (#16)
    -created interface UserDAO with javadoc
    -created implementation of that interface

--- Monsters hunting persistence/src/main/java/cz/fi/muni/pa165/dao/UserDao.java
+++ Monsters hunting persistence/src/main/java/cz/fi/muni/pa165/dao/UserDao.java
@@ -4,10 +4,49 @@ import cz.fi.muni.pa165.entity.User;
 
 import java.util.List;
 
+/**
+ * Interface of UserDao
+ * @author Balcirak Peter <peter.balcirak@gmail.com>
+ */
 public interface UserDao {
+
+	/**
+	 * Creates user in database
+	 * @param user which will be created
+	 *
+	 * @throws IllegalArgumentException when given parameter is null
+	 */
 	void create(User user);
+
+	/**
+	 * Deletes user from database
+	 * @param user which will be deleted
+	 *
+	 * @throws IllegalArgumentException when given parameter is null
+	 */
 	void delete(User user);
+
+	/**
+	 * Finds user in database by his id and returns him as object User
+	 * @param id by which will be user find
+	 * @return User or null if there is not user with that id
+	 *
+	 * @throws IllegalArgumentException when given parameter is null or less than 0
+	 */
 	User findById(Long id);
+
+	/**
+	 * Finds user in database by his email and returns him as object User
+	 * @param email by which will be user find
+	 * @return User or null if there is not user whit that email
+	 *
+	 * @throws IllegalArgumentException when given parameter is null or empty
+	 */
 	User findByEmail(String email);
+
+	/**
+	 * Gets all users stored in database
+	 * @return List of all Users in database or empty List if there are no users
+	 */
 	List<User> getAll();
 }
--- Monsters hunting persistence/src/main/java/cz/fi/muni/pa165/dao/UserDaoImpl.java
+++ Monsters hunting persistence/src/main/java/cz/fi/muni/pa165/dao/UserDaoImpl.java
@@ -1,35 +1,73 @@
 package cz.fi.muni.pa165.dao;
 
 import cz.fi.muni.pa165.entity.User;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.NoResultException;
 import java.util.List;
 
+/**
+ * Implementation of UserDao interface
+ * @author Balcirak Peter <peter.balcirak@gmail.com>
+ */
 @Named
 public class UserDaoImpl implements UserDao {
+
+	@PersistenceContext
+	private EntityManager em;
+
 	@Override
 	public void create(User user) {
-		throw new NotImplementedException();
+		if(user == null){
+			throw new IllegalArgumentException("Parameter User cannot be null!");
+		}
+		em.persist(user);
+		em.flush();
 	}
 
 	@Override
 	public void delete(User user) {
-		throw new NotImplementedException();
+		if(user == null){
+			throw new IllegalArgumentException("Parameter User cannot be null!");
+		}
+		em.remove(user);
+		em.flush();
 	}
 
 	@Override
 	public User findById(Long id) {
-		throw new NotImplementedException();
+		if(id == null){
+			throw new IllegalArgumentException("Parameter id cannot be null!");
+		}
+		if(id < 0){
+			throw new IllegalArgumentException("Parameter id cannot be less than 0!");
+		}
+		return em.find(User.class, id);
 	}
-	
+
 	@Override
 	public User findByEmail(String email) {
-		throw new NotImplementedException();
+		if(email == null){
+			throw new IllegalArgumentException("Parameter email cannot be null!");
+		}
+		if(email.isEmpty()){
+			throw new IllegalArgumentException("Parameter email cannot be empty!");
+		}
+		try {
+			return em
+					.createQuery("select u from User u where email = :email",
+							User.class).setParameter("email", email)
+					.getSingleResult();
+		} catch (NoResultException nrf) {
+			return null;
+		}
 	}
 
 	@Override
 	public List<User> getAll() {
-		throw new NotImplementedException();
+		return em.createQuery("SELECT u FROM User u",
+				User.class).getResultList();
 	}
 }
