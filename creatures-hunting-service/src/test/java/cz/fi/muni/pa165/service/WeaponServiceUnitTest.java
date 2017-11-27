1545fe4204895b86c706030375fdcf3ddb6f0892
Pavel Vyskočil <vyskocilpavel@muni.cz>
2017-11-27 12:48:28 +0100

    Add tests (#45)
    * WeaponFacadeImpl
    
    * Added implementation of WeaponFacade
    
    * Add javadoc
    
    * WeaponServiceImpl
    
    * Added implementation of WeaponService
    
    * Add javadoc for ServiceLayer
    
    * WeaponFacadeTest
    
    * Added tests for WeaponFacade
    
    * WeaponServiceTest
    
    * Added tests for Weapon Service

---
+++ creatures-hunting-service/src/test/java/cz/fi/muni/pa165/service/WeaponServiceUnitTest.java
@@ -0,0 +1,154 @@
+package cz.fi.muni.pa165.service;
+
+import cz.fi.muni.pa165.dao.MonsterDao;
+import cz.fi.muni.pa165.dao.WeaponDao;
+import cz.fi.muni.pa165.entity.Monster;
+import cz.fi.muni.pa165.entity.Weapon;
+import cz.fi.muni.pa165.enums.WeaponType;
+import org.mockito.InjectMocks;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class WeaponServiceUnitTest {
+
+    private WeaponDao weaponDao = mock(WeaponDao.class);
+
+    private MonsterDao monsterDao = mock(MonsterDao.class);
+
+    @InjectMocks
+    private WeaponService weaponService;
+
+    private Weapon pistol;
+    private Weapon rifle;
+    private Weapon shotgun;
+
+    private Monster spider;
+    private Monster zombie;
+
+    @BeforeMethod
+    public void setService() {
+        weaponService = new WeaponServiceImpl(weaponDao, monsterDao);
+    }
+
+    @BeforeMethod
+    public void createEntities(){
+        spider = new Monster("Spider");
+        zombie = new Monster("Zombie");
+
+        pistol = new Weapon("Pistol");
+        pistol.setId(1L);
+        pistol.setRange(100);
+        pistol.setMagazineCapacity(10);
+        pistol.setType(WeaponType.PISTOL);
+
+        rifle = new Weapon("Rifle");
+        rifle.setId(2L);
+        rifle.setRange(150);
+        rifle.setMagazineCapacity(25);
+        rifle.setType(WeaponType.RIFLE);
+
+        shotgun = new Weapon("Shotgun");
+        shotgun.setId(3L);
+        shotgun.setRange(80);
+        shotgun.setMagazineCapacity(2);
+        shotgun.setType(WeaponType.SHOTGUN);
+
+        pistol.addAppropriateMonster(spider);
+        rifle.addAppropriateMonster(spider);
+        rifle.addAppropriateMonster(zombie);
+        shotgun.addAppropriateMonster(zombie);
+    }
+
+    @Test
+    public void testCreateWeapon(){
+        weaponService.createWeapon(pistol);
+
+        verify(weaponDao, times(1)).create(pistol);
+    }
+
+    @Test
+    public void testDeleteWeapon(){
+        when(weaponDao.findById(1L)).thenReturn(pistol);
+
+        weaponService.deleteWeapon(pistol);
+
+        verify(weaponDao, times(1)).delete(pistol);
+    }
+
+    @Test
+    public void testFindWeaponById(){
+        when(weaponDao.findById(1L)).thenReturn(pistol);
+
+        Weapon weapon = weaponService.findById(1L);
+
+        assertThat(weapon).isEqualToComparingFieldByField(pistol);
+    }
+
+    @Test
+    public void testFindWeaponByName(){
+        when(weaponDao.findByName("Pistol")).thenReturn(pistol);
+
+        Weapon weapon = weaponService.findByName("Pistol");
+
+        assertThat(weapon).isEqualToComparingFieldByField(pistol);
+    }
+
+    @Test
+    public void testGetAllWeapons(){
+        when(weaponDao.getAll()).thenReturn(Arrays.asList(pistol, rifle, shotgun));
+
+        List<Weapon> weapons = weaponService.getAllWeapons();
+
+        assertThat(weapons).containsOnly(pistol, rifle, shotgun);
+    }
+
+    @Test
+    public void testGetAllWeaponsByType(){
+        when(weaponDao.getAllForType(WeaponType.SHOTGUN)).thenReturn(Arrays.asList(shotgun));
+
+        List<Weapon> weapons = weaponService.getAllForType(WeaponType.SHOTGUN);
+
+        assertThat(weapons).containsOnly(shotgun);
+    }
+
+    @Test
+    public void testAddAppropriateMonster(){
+        when(weaponDao.findById(pistol.getId())).thenReturn(pistol);
+        when(monsterDao.findById(zombie.getId())).thenReturn(zombie);
+
+        weaponService.addAppropriateMonster(pistol.getId(), zombie.getId());
+
+        assertThat(pistol.getAppropriateMonsters()).containsOnly(spider, zombie);
+
+    }
+
+    @Test
+    public void testRemoveAppropriateMonster(){
+        when(weaponDao.findById(pistol.getId())).thenReturn(pistol);
+        when(monsterDao.findById(spider.getId())).thenReturn(spider);
+
+        assertThat(pistol.getAppropriateMonsters()).containsOnly(spider);
+
+        weaponService.removeAppropriateMonster(pistol.getId(), spider.getId());
+
+        assertThat(pistol.getAppropriateMonsters()).doesNotContain(spider);
+    }
+
+    @Test
+    public void testGetTheMostEffectiveWeapon(){
+        when(weaponDao.getAll()).thenReturn(Arrays.asList(pistol, rifle, shotgun));
+
+        List<Weapon> mostEffectiveWeapons = weaponService.getTheMostEffectiveWeapon();
+
+        assertThat(mostEffectiveWeapons).containsOnly(rifle);
+    }
+}
---
+++ creatures-hunting-service/src/test/java/cz/fi/muni/pa165/service/facade/WeaponFacadeUnitTest.java
@@ -0,0 +1,230 @@
+package cz.fi.muni.pa165.service.facade;
+
+import cz.fi.muni.pa165.dto.MonsterDTO;
+import cz.fi.muni.pa165.dto.WeaponCreateDTO;
+import cz.fi.muni.pa165.dto.WeaponDTO;
+import cz.fi.muni.pa165.entity.Monster;
+import cz.fi.muni.pa165.entity.Weapon;
+import cz.fi.muni.pa165.enums.MonsterAgility;
+import cz.fi.muni.pa165.enums.WeaponType;
+import cz.fi.muni.pa165.facade.WeaponFacade;
+import cz.fi.muni.pa165.service.BeanMappingService;
+import cz.fi.muni.pa165.service.MonsterService;
+import cz.fi.muni.pa165.service.WeaponService;
+import cz.fi.muni.pa165.service.config.ServiceConfiguration;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Implementation of Facade Unit tests
+ *
+ * @author Pavel Vyskocil <vyskocilpavel@muni.cz>
+ */
+public class WeaponFacadeUnitTest{
+
+    private WeaponService weaponService = mock(WeaponService.class);
+
+    private MonsterService monsterService = mock(MonsterService.class);
+
+    private BeanMappingService beanMappingService = mock(BeanMappingService.class);
+
+    @InjectMocks
+    private WeaponFacade weaponFacade;
+
+    private Weapon pistol;
+    private Weapon shotgun;
+    private Weapon rifle;
+
+    private WeaponDTO pistolDTO;
+    private WeaponDTO shotgunDTO;
+    private WeaponDTO rifleDTO;
+
+    private WeaponCreateDTO pistolCreateDTO;
+
+    private Monster zombie;
+
+    private MonsterDTO zombieDTO;
+
+    @BeforeMethod
+    public void setFacade(){
+        weaponFacade = new WeaponFacadeImpl(weaponService, beanMappingService);
+    }
+
+    @BeforeMethod
+    public void createEntities(){
+        pistol = new Weapon("Pistol");
+        pistol.setRange(100);
+        pistol.setMagazineCapacity(10);
+        pistol.setType(WeaponType.PISTOL);
+
+        rifle = new Weapon("Rifle");
+        rifle.setId(2L);
+        rifle.setRange(150);
+        rifle.setMagazineCapacity(25);
+        rifle.setType(WeaponType.RIFLE);
+
+        shotgun = new Weapon("Shotgun");
+        shotgun.setId(3L);
+        shotgun.setRange(80);
+        shotgun.setMagazineCapacity(2);
+        shotgun.setType(WeaponType.SHOTGUN);
+
+        pistolDTO = new WeaponDTO();
+        pistolDTO.setName("Pistol");
+        pistolDTO.setId(1L);
+        pistolDTO.setRange(100);
+        pistolDTO.setMagazineCapacity(10);
+        pistolDTO.setType(WeaponType.PISTOL);
+
+        rifleDTO = new WeaponDTO();
+        rifleDTO.setName("Rifle");
+        rifleDTO.setId(2L);
+        rifleDTO.setRange(150);
+        rifleDTO.setMagazineCapacity(25);
+        rifleDTO.setType(WeaponType.RIFLE);
+
+        shotgunDTO = new WeaponDTO();
+        shotgunDTO.setName("Shotgun");
+        shotgunDTO.setId(3L);
+        shotgunDTO.setRange(80);
+        shotgunDTO.setMagazineCapacity(2);
+        shotgunDTO.setType(WeaponType.SHOTGUN);
+
+        pistolCreateDTO = new WeaponCreateDTO();
+        pistolCreateDTO.setName("Pistol");
+        pistolCreateDTO.setRange(100);
+        pistolCreateDTO.setMagazineCapacity(10);
+        pistolCreateDTO.setType(WeaponType.PISTOL);
+
+        zombie = new Monster();
+        zombie.setId(1L);
+        zombie.setName("Zombie");
+        zombie.setHeight(54.0);
+        zombie.setWeight(105.0);
+        zombie.setAgility(MonsterAgility.FAST);
+
+        zombieDTO = new MonsterDTO();
+        zombieDTO.setId(1L);
+        zombieDTO.setName(zombie.getName());
+        zombieDTO.setHeight(zombie.getHeight());
+        zombieDTO.setWeight(zombie.getWeight());
+        zombieDTO.setAgility(zombie.getAgility());
+
+        rifleDTO.addAppropriateMonster(zombieDTO);
+
+    }
+
+    @Test
+    public void testCreateWeapon(){
+        when(beanMappingService.mapTo(pistolCreateDTO, Weapon.class)).thenReturn(pistol);
+
+        doAnswer(invocation -> {
+            pistol.setId(1L);
+            return null;
+        }).when(weaponService).createWeapon(pistol);
+
+        Long id = weaponFacade.createWeapon(pistolCreateDTO);
+
+        verify(weaponService, times(1)).createWeapon(pistol);
+
+        assertThat(id).isEqualTo(1L);
+    }
+
+    @Test
+    public void testDeleteWeapon(){
+        Long id = 1L;
+
+        when(weaponService.findById(id)).thenReturn(pistol);
+
+        weaponFacade.deleteWeapon(id);
+
+        verify(weaponService, times(1)).deleteWeapon(pistol);
+    }
+
+    @Test
+    public void testFindWeaponById(){
+        Long id = 1L;
+
+        when(weaponService.findById(id)).thenReturn(pistol);
+        when(beanMappingService.mapTo(pistol,WeaponDTO.class)).thenReturn(pistolDTO);
+
+        WeaponDTO weaponDTO = weaponFacade.findById(id);
+
+        assertThat(weaponDTO).isEqualToComparingFieldByField(pistolDTO);
+    }
+
+    @Test
+    public void testFindWeaponByName(){
+        String name = "Pistol";
+
+        when(weaponService.findByName(name)).thenReturn(pistol);
+        when(beanMappingService.mapTo(pistol,WeaponDTO.class)).thenReturn(pistolDTO);
+
+        WeaponDTO weaponDTO = weaponFacade.findByName(name);
+
+        assertThat(weaponDTO).isEqualToComparingFieldByField(pistolDTO);
+    }
+
+    @Test
+    public void testGetAllWeapons(){
+        when(weaponService.getAllWeapons()).thenReturn(Arrays.asList(pistol,rifle,shotgun));
+        when(beanMappingService.mapTo(any(), eq(WeaponDTO.class))).thenReturn(Arrays.asList(pistolDTO, rifleDTO, shotgunDTO));
+
+        assertThat(weaponFacade.getAll()).containsOnly(pistolDTO, rifleDTO, shotgunDTO);
+    }
+
+    @Test
+    public void testGetAllWeaponsForType(){
+        when(weaponService.getAllForType(WeaponType.RIFLE)).thenReturn(Arrays.asList(rifle));
+        when(beanMappingService.mapTo(any(), eq(WeaponDTO.class))).thenReturn(Arrays.asList(rifleDTO));
+
+        assertThat(weaponFacade.getAllForType(WeaponType.RIFLE)).containsOnly(rifleDTO);
+    }
+
+    @Test
+    public void testAddAppropriateMonster(){
+        Long id = 1L;
+
+        when(weaponService.findById(id)).thenReturn(pistol);
+        when(monsterService.findById(id)).thenReturn(zombie);
+
+        weaponFacade.addAppropriateMonster(id, id);
+
+        verify(weaponService, times(1)).addAppropriateMonster(id, id);
+    }
+
+    @Test
+    public void testRemoveAppropriateMonster(){
+        Long weaponId = 2L;
+        Long monsterId = 1L;
+
+        when(weaponService.findById(weaponId)).thenReturn(rifle);
+        when(monsterService.findById(monsterId)).thenReturn(zombie);
+
+        weaponFacade.removeAppropriateMonster(weaponId, monsterId);
+
+        verify(weaponService, times(1)).removeAppropriateMonster(weaponId, monsterId);
+    }
+
+    @Test
+    public void testGetTheMostEffectiveWeapon(){
+        when(weaponService.getTheMostEffectiveWeapon()).thenReturn(Arrays.asList(rifle));
+        when(beanMappingService.mapTo(any(), eq(WeaponDTO.class))).thenReturn(Arrays.asList(rifleDTO));
+
+        assertThat(weaponFacade.getTheMostEffectiveWeapon()).containsOnly(rifleDTO);
+    }
+}
