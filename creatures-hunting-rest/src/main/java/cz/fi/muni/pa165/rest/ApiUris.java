e36d93a338bbbc59628946baf3f2e7c351a275b0
Pavel Vyskočil <vyskocilpavel@muni.cz>
2017-12-09 22:55:21 +0100

    Rest (#51)
    * Added methods for update weapons
    
    * Added WeaponUpdateDTO
    * Added test for update method
    
    * Weapon REST
    
    * Added Weapon Controller
    * Added Tests for Weapon Controller

--- creatures-hunting-rest/src/main/java/cz/fi/muni/pa165/rest/ApiUris.java
+++ creatures-hunting-rest/src/main/java/cz/fi/muni/pa165/rest/ApiUris.java
@@ -2,7 +2,9 @@ package cz.fi.muni.pa165.rest;
 
 /**
  * @author Vojtech Sassmann &lt;vojtech.sassmann@gmail.com&gt;
+ * @author Pavel Vyskocil <vyskocilpavel@muni.cz>
  */
 public class ApiUris {
 	public static final String ROOT_URI_MONSTERS = "/monsters";
+	public static final String ROOT_URI_WEAPONS = "/weapons";
 }
